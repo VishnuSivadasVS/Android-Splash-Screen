9c99cfe4520df5d14ecc0cbb199472b1a19c01c6
Vishnu <mail@vishnusivadas.com>
2019-05-05 00:32:16 +0530

    Initial commit

---
+++ app/src/main/java/com/codeseasy/splashscreencorrectway/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.codeseasy.splashscreencorrectway;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
